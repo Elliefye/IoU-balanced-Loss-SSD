54211770e38a8b3de7fba2d4b8a9dfa242564dfa
Elina <elina815@gmail.com>
2021-04-25 17:48:08 +0300

    removed unused loss

--- model/loss.py
+++ model/loss.py
@@ -9,47 +9,6 @@ import torch.nn.functional as F
 from model.utils import Encoder, generate_dboxes
 
 
-class SigmoidDRLoss(nn.Module):
-    def __init__(self, pos_lambda=1, neg_lambda=0.1/math.log(3.5), L=6., tau=4.):
-        super(SigmoidDRLoss, self).__init__()
-        self.margin = 0.5
-        self.pos_lambda = pos_lambda
-        self.neg_lambda = neg_lambda
-        self.L = L
-        self.tau = tau
-
-    def forward(self, logits, targets, classes):
-        t = torch.zeros(4, 4, 8732)
-        tar = targets.unsqueeze(1)
-
-        # pad ground truth with 0
-        for i in range(len(classes)):
-            t[i][classes[i]] = tar[i]
-
-        # num_classes = logits.shape[1]
-        # dtype = targets.dtype
-        # device = targets.device
-        # class_range = torch.arange(1, num_classes + 1, dtype=dtype, device=device).unsqueeze(0)
-        class_range = [0, 1, 2, 3]
-
-        pos_ind = (t == class_range)
-        # pos_ind = t > 0
-        neg_ind = (t != class_range) * (t >= 0)
-        pos_prob = logits[pos_ind].sigmoid()
-        neg_prob = logits[neg_ind].sigmoid()
-        # print(pos_prob)
-        # print(neg_prob)
-        neg_q = F.softmax(neg_prob/self.neg_lambda, dim=0)
-        neg_dist = torch.sum(neg_q * neg_prob)
-        if pos_prob.numel() > 0:
-            pos_q = F.softmax(-pos_prob/self.pos_lambda, dim=0)
-            pos_dist = torch.sum(pos_q * pos_prob)
-            loss = self.tau*torch.log(1.+torch.exp(self.L*(neg_dist - pos_dist+self.margin)))/self.L
-        else:
-            loss = self.tau*torch.log(1.+torch.exp(self.L*(neg_dist - 1. + self.margin)))/self.L
-        return loss
-
-
 def compute_iou(a, b):
     """
     Parameters
